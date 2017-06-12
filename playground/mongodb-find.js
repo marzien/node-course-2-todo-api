eb6992e9a9f3a7a575c043b46b2cfbea779b9cf1
marzien <mariuszienius@gmail.com>
2017-06-13 01:11:26 +0300

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connecto to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('593f09a8d5d0cc50c96168be')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todo', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todo', err);
+    // });
+
+    db.collection('Users').find({
+        name: 'Marius'
+    }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todo', err);
+    });
+
+
+    // db.close();
+});
